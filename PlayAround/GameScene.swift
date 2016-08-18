038bba19a5bfc55f89904c1e8492bfb50dc26fc2
Michael <mredig@gmail.com>
2016-08-18 16:41:32 -0500

    about to iOS

--- PlayAround/GameScene.swift
+++ PlayAround/GameScene.swift
@@ -8,33 +8,65 @@
 
 import SpriteKit
 
+var ninjaIdleAction = SKAction()// <------
+
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+		
+		self.backgroundColor = SKColor.init(red: 0.2, green: 0.0, blue: 0.2, alpha: 0.2)
+
+		let ninjaIdleAtlas = SKTextureAtlas(named: "NinjaIdle")// <------
+		
+		var ninjaIdleArray = [SKTexture]()// <------
+		for textureName in ninjaIdleAtlas.textureNames {// <------
+			ninjaIdleArray.append(ninjaIdleAtlas.textureNamed(textureName))// <------
+		}// <------
+		
+		ninjaIdleAction = SKAction.animateWithTextures(ninjaIdleArray, timePerFrame: 1.0/12.0)// <------
+		ninjaIdleAction = SKAction.repeatActionForever(ninjaIdleAction)// <------
+		
+		
+		let ninja = SKSpriteNode(imageNamed: "Idle_000")
+//		ninja.setScale(0.25)
+		
+		ninja.name = "theNinja"
+		
+		ninja.runAction(ninjaIdleAction)// <------
+		self.addChild(ninja)
+		
+		
     }
     
     override func mouseDown(theEvent: NSEvent) {
         /* Called when a mouse click occurs */
         
         let location = theEvent.locationInNode(self)
-        
-        let sprite = SKSpriteNode(imageNamed:"Spaceship")
-        sprite.position = location;
-        sprite.setScale(0.5)
-        
-        let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-        sprite.runAction(SKAction.repeatActionForever(action))
-        
-        self.addChild(sprite)
-    }
-    
+		
+		let ninja = self.childNodeWithName("theNinja") // <----
+		ninja?.position = location // <-----
+		
+	
+	}
+	
+	override func mouseDragged(theEvent: NSEvent) {
+		let location = theEvent.locationInNode(self)
+		
+		let ninja = self.childNodeWithName("theNinja") // <----
+		ninja?.position = location // <-----
+	}
+	
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
 }
+
+
+
+
+
+
+
+
+
+
